b5770132786c43813c656998953e5bf80a473aaf
Juan Fernando Echavarria Zapata <fernando.echavarria.ies@gmail.com>
2019-06-05 08:00:15 -0500

    commit inicial

---
+++ src/main/java/co/com/ies/ServiceSimulatorApplication.java
@@ -0,0 +1,13 @@
+package co.com.ies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSimulatorApplication.class, args);
+	}
+
+}
