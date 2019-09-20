6b5afdc2110bc573e1284dee270afc11f4805821
Juan Fernando Echavarria Zapata <fernando.echavarria.ies@gmail.com>
2019-09-20 12:53:35 -0500

    style|refactor: SimuladorOperador: Solo se validan credenciales, se retorna platformTransactionId y se contesta con el playerId. Los demas datos son quemados.

--- src/main/java/co/com/ies/web/rest/api/SimulatorOperatorController.java
+++ src/main/java/co/com/ies/web/rest/api/SimulatorOperatorController.java
@@ -34,17 +34,11 @@ import co.com.ies.service.dto.sub.AuthDto;
 @RestController
 @RequestMapping("/simuladoroperador")
 public class SimulatorOperatorController {
-  private static final int ERROR_ID_102 = 102;
-  private static final int ERROR_ID_29 = 29;
-  private static final int ERROR_ID_109 = 109;
-  private static final int ERROR_ID_21 = 21;
   private static final String VOID = "doesn't apply";
   private static final BigDecimal ZERO_BD = BigDecimal.ZERO;
   private static final Integer ZERO_I = 0;
   private static final String USER = "inWPLAYIesUser";
   private static final String PASS = "pass";
-  private static final String PLAYER_ID = "playerId";
-  private static final BigDecimal TOTAL_BALANCE = BigDecimal.valueOf(1000000);
   private static final String TOKEN = "123t2o2k2e2n123";
   private static final Integer HAS_ERROR_NOT = 0;
   private static final Integer HAS_ERROR = 1;
@@ -52,15 +46,10 @@ public class SimulatorOperatorController {
   private static final Integer ERROR_ID_SOME = -1;
   private static final String ERROR_DESCRIPTION = VOID;
   private static final String ERROR_DESCRIPTION_SOME = "Bab credentials";
-  private static final String ERROR_DESCRIPTION_102 = "Not registered";
-  private static final String ERROR_DESCRIPTION_21 = "No tienes saldo suficiente";
-  private static final String ERROR_DESCRIPTION_21_CODE = "El código ya fue redimido";
-  private static final String ERROR_DESCRIPTION_29 = "Estás bloqueado";
-  private static final String ERROR_DESCRIPTION_109 = "El código es erróneo";
   private static final String PLATFORM_TRANSACTION_ID = "wplay";
+  private static final BigDecimal AMOUNT = new BigDecimal(3000);
+  private static final Integer NUM_CARDS = 3;
   private static final AuthDto auth = new AuthDto().setUser(USER).setPass(PASS);
-  private Map<String, BigDecimal> totalBalances = new HashMap<>();
-  private Map<String, BigDecimal> transactions = new HashMap<>();
   private Map<String, String> players = new HashMap<>();
 
   /**
@@ -95,7 +84,6 @@ public class SimulatorOperatorController {
   @ResponseBody
   public DebitAndCreditOperatorInDto debitAndCredit(@Valid @RequestBody DebitAndCreditOperatorOutDto operatorOut) {
     String token = operatorOut.getToken();
-    
     if (!auth.equals(operatorOut.getAuth())) {
       return debitAndCreditErrorStructureWithCodeAndMsg(ERROR_ID_SOME, ERROR_DESCRIPTION_SOME);
     }
@@ -119,36 +107,13 @@ public class SimulatorOperatorController {
   @ResponseBody
   public DebitAndCreditCodeOperatorInDto debitAndCreditCode(@Valid @RequestBody DebitAndCreditCodeOperatorOutDto operatorOut) {
     String token = operatorOut.getToken();
-    
     if (!auth.equals(operatorOut.getAuth())) {
       return debitAndCreditCodeErrorStructureWithCodeAndMsg(ERROR_ID_SOME, ERROR_DESCRIPTION_SOME);
     }
-    BigDecimal totalBalance = totalBalances.get(players.get(token));
-    if (Objects.isNull(totalBalance)) {
-      return debitAndCreditCodeErrorStructureWithCodeAndMsg(ERROR_ID_102, ERROR_DESCRIPTION_102);
-    }
-    String code = operatorOut.getCode();
-    Integer numCards;
-    BigDecimal amount;
-    try {
-      numCards = Integer.valueOf(code.split("-")[0]);
-      amount = new BigDecimal(code.split("-")[1]); 
-    } catch (Exception e) {
-      return debitAndCreditCodeErrorStructureWithCodeAndMsg(ERROR_ID_109, ERROR_DESCRIPTION_109);
-    }
-
-    BigDecimal transactionValue = BigDecimal.ZERO;
-    totalBalance = totalBalance.add(transactionValue);    
-    if (totalBalance.compareTo(BigDecimal.ZERO) < 0 || numCards.equals(0)) {
-      return debitAndCreditCodeErrorStructureWithCodeAndMsg(ERROR_ID_21, ERROR_DESCRIPTION_21_CODE);
-    }
-
-    totalBalances.put(players.get(token), totalBalance);
-    transactions.put(operatorOut.getIesTransactionId(), transactionValue);
     return new DebitAndCreditCodeOperatorInDto()
-        .setTotalBalance(totalBalances.get(players.get(token)))
-        .setAmount(amount)
-        .setNumCards(numCards)
+        .setTotalBalance(BigDecimal.TEN)
+        .setAmount(AMOUNT)
+        .setNumCards(NUM_CARDS)
         .setPlayerId(players.get(token))
         .setToken(token)
         .setHasError(HAS_ERROR_NOT)
@@ -177,13 +142,8 @@ public class SimulatorOperatorController {
           .setErrorDescription(ERROR_DESCRIPTION_SOME)
           .setPlatformTransactionId(VOID);
     }
-    BigDecimal transactionValue = transactions.get(operatorOut.getIesRelatedTransactionId()).negate();
-    BigDecimal totalBalance = totalBalances.get(operatorOut.getPlayerId()).add(transactionValue);
-    totalBalances.put(operatorOut.getPlayerId(), totalBalance);
-    transactions.put(operatorOut.getIesTransactionId(), transactionValue);
-  
     return new RollbackOperatorInDto()
-        .setTotalBalance(totalBalances.get(operatorOut.getPlayerId()))
+        .setTotalBalance(BigDecimal.TEN)
         .setPlayerId(operatorOut.getPlayerId())
         .setToken(TOKEN)
         .setHasError(HAS_ERROR_NOT)
@@ -202,8 +162,6 @@ public class SimulatorOperatorController {
   @ResponseBody
   public GetBalanceOperatorInDto getBalance(
       @Valid @RequestBody GetBalanceOperatorOutDto operatorOut) {
-    System.out.println(ZonedDateTime.now());
-    System.out.println(operatorOut);
     String token = operatorOut.getToken();
     if(!auth.equals(operatorOut.getAuth())) {
       return new GetBalanceOperatorInDto()
@@ -214,11 +172,8 @@ public class SimulatorOperatorController {
           .setErrorId(ERROR_ID_SOME)
           .setErrorDescription(ERROR_DESCRIPTION_SOME);
     }
-    System.out.println("player ByToken : "+ players.get(token));   
-    System.out.println("current player balances: ");  
-    totalBalances.forEach((k,v)->{System.out.println(k+"\t"+v);});
     return new GetBalanceOperatorInDto()
-        .setTotalBalance(totalBalances.get(players.get(token)))
+        .setTotalBalance(BigDecimal.TEN)
         .setPlayerId(players.get(token))
         .setToken(token)
         .setHasError(HAS_ERROR_NOT)
